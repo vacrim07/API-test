0eb9cb6faa02aceb6dc6166d6eb7e562556d2881
vacrim07 <vacrim07@gmail.com>
2019-11-20 08:40:30 -0500

    testing schema

--- src/test/java/api/CommentTest.java
+++ src/test/java/api/CommentTest.java
@@ -1,27 +1,24 @@
 package api;
 
-import com.google.gson.Gson;
-import com.jayway.restassured.RestAssured;
-import model.Comment;
-import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import static com.jayway.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.apache.http.HttpStatus.SC_OK;
 
 public class CommentTest {
 
-    private static Gson gson = new Gson();
-    private static String body = "est rerum tempore vitae\n" +
-            "sequi sint nihil reprehenderit dolor beatae ea dolores neque\n" +
-            "fugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\n" +
-            "qui aperiam non debitis possimus qui neque nisi nulla";
+    private final static String BASIC_PATH = "https://jsonplaceholder.typicode.com/";
 
     @Test
-    public static void getComment(){
-        String url = "https://jsonplaceholder.typicode.com/posts/2";
-        String response = RestAssured.given().when().get(url).asString();
-        Comment comment = gson.fromJson(response, Comment.class);
-        Assert.assertEquals(comment.getUserId(), new Integer(1));
-        Assert.assertEquals(comment.getId(), new Integer(2));
-        Assert.assertEquals(comment.getTitle(), "qui est esse");
-        Assert.assertEquals(comment.getBody(), body);
+    public static void shouldGetAComment() {
+        String endpoint = "posts/2";
+        given()
+                .when()
+                .get(BASIC_PATH + endpoint)
+                .then()
+                .assertThat()
+                .statusCode(SC_OK)
+                .body(matchesJsonSchemaInClasspath("schemas/api/comment.json"));
     }
 }
